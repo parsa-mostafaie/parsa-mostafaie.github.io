23bf8a8da897e626563aae6be36683ac55f4234b
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-01-16 18:53:29 +0330

    Fix bugs
    Now, when clicking on the logo on large screens, the 'shown' class is no longer added or removed from the classlist of navbar.

--- scripts/app.js
+++ scripts/app.js
@@ -15,3 +15,25 @@ var timeBetween = 50;
 appearChars(str, elem, timeBetween);
 
 var ulMenuP = document.querySelector("nav");
+
+function change_menu_state() {
+  var media_query = window.matchMedia("(max-width: 768px)");
+  var matchMedia = media_query.matches;
+
+  if (matchMedia) {
+    ulMenuP.classList.toggle("shown");
+  } else {
+    ulMenuP.classList.remove("shown");
+  }
+}
+
+function check_menu_state() {
+  var media_query = window.matchMedia("(max-width: 768px)");
+  var matchMedia = media_query.matches;
+
+  if (!matchMedia) {
+    ulMenuP.classList.remove("shown");
+  }
+}
+
+window.addEventListener("resize", check_menu_state);
