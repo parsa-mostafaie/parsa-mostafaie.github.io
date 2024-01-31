d7ac51a7b6532103f59920c8d34960249c6c90c6
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-01-31 10:22:20 +0330

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -93,3 +93,24 @@ function autoTheme(e) {
     }
   }
 }
+
+// Scroll Direction
+{
+  let lastScrollTop = 0;
+
+  // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+  window.addEventListener(
+    "scroll",
+    function () {
+      // or window.addEventListener("scroll"....
+      var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+      if (st > lastScrollTop) {
+        chngthBTN.style.display = "none";
+      } else if (st < lastScrollTop) {
+        chngthBTN.style.display = "block";
+      } // else was horizontal scroll
+      lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+    },
+    false
+  );
+}
