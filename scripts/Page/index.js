c4c8de3b167a03ec174086bfb15fafc18247509e
Parsa Mostafaie <155385612+parsa-mostafaie@users.noreply.github.com>
2024-02-20 17:48:45 +0330

    use Swiper For Skills Section

--- scripts/Page/index.js
+++ scripts/Page/index.js
@@ -6,3 +6,26 @@ var delay = 10;
 window.addEventListener("load", () =>
   appearChars(str, elem, timeBetween, delay)
 );
+
+// swiper
+const swiper = new Swiper(".swiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  centeredSlides: true,
+  slidesPerView: 1,
+  spaceBetween: 25,
+  breakpoints: {
+    // when window width is >= 800px
+    800: {
+      slidesPerView: 2,
+      centeredSlides: true
+    },
+  },
+  loop: true,
+});
